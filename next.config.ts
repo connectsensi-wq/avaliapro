132beb27232498612287e8a264694e12aabf603d
José Wilbson Alves Pereira <sensiconnect@MacBook-Air-de-Jose.local>
2026-02-26 15:42:57 -0300

    Edit next.config.ts add eslintignore

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images : {
-    remotePatterns : [{hostname: "images.pexels.com"}]
-  }
+  images: {
+    remotePatterns: [{ hostname: "images.pexels.com" }],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
